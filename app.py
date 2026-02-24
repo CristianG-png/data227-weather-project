7c488e59d4f01058db8529fa9cb2d921ddb46309
CristianG-png <155701238+CristianG-png@users.noreply.github.com>
2026-02-24 16:39:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.set_page_config(page_title="Analysis of Weather Patterns in Seattle:", layout="wide")
 
 st.title("Analysis of Weather Patterns in Seattle")
-st.write(Image.open('images/seattle-weather.jpg'))
+st.write(Image.open('images/SeattleNeighborhoodMap.jpg'))
 st.write("This project will serve as a demonstration of a Narrative Visualization project with charts\
          designed using Altair. This project is deployed using Streamlit.\n")
 st.write(
@@ -13,5 +13,4 @@ st.write(
     "- **Explore**: For a closer reader-driven exploration of the data, we provide a few interactive designs.\n"
     "- **Methods**: We lay down some key details about our data and limitations to our analysis.\n"
 )
-st.write(Image.open('images/SeattleNeighborhoodMap.jpg'))
 st.info("Dataset: `vega_datasets.data.seattle_weather()`")
