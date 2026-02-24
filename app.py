f6fd396e84e6c3ae9042e6b725cfea414d9bf6ba
CristianG-png <155701238+CristianG-png@users.noreply.github.com>
2026-02-24 16:34:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Analysis of Weather Patterns in Seattle", layout="wide")
+st.set_page_config(page_title="Analysis of Weather Patterns in Seattle:", layout="wide")
 
 st.title("Analysis of Weather Patterns in Seattle")
 st.write(Image.open('images/seattle-weather.jpg'))
-st.write("This project is meant to serve as a demonstration of a Narrative Visualization project\
-         where charts are designed using Altair. This project is deployed using Streamlit.\n")
+st.write("This project will serve as a demonstration of a Narrative Visualization project with charts\
+         designed using Altair. This project is deployed using Streamlit.\n")
 st.write(
     "To explore this visual data story, please navigate it through the pages in the sidebar:\n"
-    "- **Central Narrative**: We begin by taking into account daily weather patterns over time.\n"
-    "- **Exploration**: For a closer reader-driven exploration of the data, we provide a few interactive designs.\n"
-    "- **Methodology**: We lay down some key details about our data and limitations to our analysis.\n"
+    "- **Story**: The central narrative, beginning with taking into account daily weather patterns over time.\n"
+    "- **Explore**: For a closer reader-driven exploration of the data, we provide a few interactive designs.\n"
+    "- **Methods**: We lay down some key details about our data and limitations to our analysis.\n"
 )
 st.write(Image.open('images/SeattleNeighborhoodMap.jpg'))
 st.info("Dataset: `vega_datasets.data.seattle_weather()`")
